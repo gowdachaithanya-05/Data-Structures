a88d4f119dfb02523def5cd9e6252113ae53f105
gowdachaithanya-05 <chaithanyargowda2003@gmail.com>
2024-10-19 22:52:49 +0530

    level - easy

---
+++ two_sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int , int > mpp;
+        int n = nums.size();
+        for (int i = 0; i<n; i++){
+            mpp[nums[i]] = i;
+        }
+        for (int i =0 ; i< n ; i++){
+            int diff = target - nums[i];
+            if(mpp.find(diff) != mpp.end() && mpp[diff] !=i){
+                return {i,mpp[diff]};
+            }
+        }
+        return { };
+    }
+    
+};
